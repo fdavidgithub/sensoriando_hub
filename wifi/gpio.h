482ec51a25f5386f2187934a6fe8b587cdf162a9
francis DAVID <contato@fdavid.com.br>
2020-09-22 21:40:48 -0300

    Change GPIO button pair for compatibility between ESP-01 and NodeMCU

--- wifi/gpio.h
+++ wifi/gpio.h
@@ -4,7 +4,12 @@
 #ifndef GPIO_H
 #define GPIO_H
 
-#define GPIO_PAIR   D0   
+#ifndef D0
+#define GPIO_PAIR   0   //ESP-01, GPIO 0
+#else
+#define GPIO_PAIR   D0  //NodeMCU, GPIO 16
+#endif
+ 
 #define GPIO_LED    LED_BUILTIN
 
 #endif
