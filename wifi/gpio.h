8f5fb288bf2763f418f400e559e3131c37a9bc95
francis DAVID <contato@fdavid.com.br>
2020-10-12 23:05:23 -0300

    New setup to send/receive commands, remove button pair

--- wifi/gpio.h
+++ wifi/gpio.h
@@ -4,12 +4,6 @@
 #ifndef GPIO_H
 #define GPIO_H
 
-#ifndef D0
-#define GPIO_PAIR   0   //ESP-01, GPIO 0
-#else
-#define GPIO_PAIR   D0  //NodeMCU, GPIO 16
-#endif
- 
 #define GPIO_LED    LED_BUILTIN
 
 #endif
--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,267 +1,257 @@
 /*
- * Build with Arduino 1.8.12
- * Board NodeMCU 1.0 (ESP-12E Module)
- * 
- */
+   Build with Arduino 1.8.12
+   Board NodeMCU 1.0 (ESP-12E Module)
+
+*/
 #include <sensoriandoData.h>
 #include <SimpleEspNowConnection.h>
 #include <time.h>
 #include "gpio.h"
 
 
-/* 
- *  MACROS
- */
+/*
+    MACROS
+*/
 //#define DEBUG
 
-#define DEBOUNCE  500
-#define TIMEOUT   1000
-#define MSTOS     1000 //Milli seconds in seconds
+#define TIMEOUT   500   //Milli seconds
+#define TIMEPAIR  120   //seconds
+#define MSTOS     1000  //Milli seconds in seconds
 
 
 /*
- * GlobalVariables
- */
+   GlobalVariables
+*/
 static SimpleEspNowConnection simpleEspConnection(SimpleEspNowRole::SERVER);
 String inputString, clientAddress;
-
 SensoriandoSensorDatum myData;
-
-#ifdef DEBUG
-  time_t DtNow = 1599788357;
-#else
-  time_t DtNow = NULL;
-#endif
-
+time_t DtNow = NULL;
 long timeelapsed;
 
 
 /*
- * Prototypes
- */
+   Prototypes
+*/
 void OnSendError(uint8_t*);
 void OnMessage(uint8_t*, const uint8_t*, size_t);
 void OnPaired(uint8_t *, String);
 void OnConnected(uint8_t *, String);
 void CmdInit(SensoriandoWifiCommandInit *);
-void SerialFlush();
 void CmdUpdate(SensoriandoWifiCommandInit *);
+void CmdPair();
 
 
 /*
- * Main
- */
-void setup() 
+   Main
+*/
+void setup()
 {
   Serial.begin(115200);
-  pinMode(GPIO_PAIR, INPUT);
-  
+
   simpleEspConnection.begin();
   simpleEspConnection.setPairingBlinkPort(GPIO_LED);
-  simpleEspConnection.onMessage(&OnMessage);  
-  simpleEspConnection.onPaired(&OnPaired);  
-  simpleEspConnection.onSendError(&OnSendError);  
-  simpleEspConnection.onConnected(&OnConnected);  
-  
+  simpleEspConnection.onMessage(&OnMessage);
+  simpleEspConnection.onPaired(&OnPaired);
+  simpleEspConnection.onSendError(&OnSendError);
+  simpleEspConnection.onConnected(&OnConnected);
+
 #ifdef DEBUG
-Serial.println();
-Serial.print("[Server] MAC ADDRESS: ");Serial.println(WiFi.macAddress());   
+  Serial.println();
+  Serial.print("[Server] MAC ADDRESS: "); Serial.println(WiFi.macAddress());
 #endif
 
   timeelapsed = millis();
 }
 
-void loop() 
+void loop()
 {
   SensoriandoWifiCommandInit cmdinit = {NULL, NULL, NULL, NULL};
-  byte stream = NULL;
+  char stream;
   byte bufstream[sizeof(cmdinit)] = {};
-  
+  long elapsedtime_serial;
+
   simpleEspConnection.loop();
-  
+
   if ( (DtNow != NULL) && ((millis() - timeelapsed) >= MSTOS) ) {
-      DtNow++; 
-      timeelapsed = millis(); 
+    DtNow++;
+    timeelapsed = millis();
 
 #ifdef DEBUG
-struct tm  ts;
-char       buf[80];
-ts = *localtime(&DtNow);
-strftime(buf, sizeof(buf), "%a %Y-%m-%d %H:%M:%S %Z", &ts);
-Serial.print(DtNow);Serial.printf(" | %s\n",buf);
+    struct tm  ts;
+    char       buf[80];
+    ts = *localtime(&DtNow);
+    strftime(buf, sizeof(buf), "%a %Y-%m-%d %H:%M:%S %Z", &ts);
+    Serial.print(DtNow); Serial.printf(" | %s\n", buf);
 #endif
   }
-  
+
   if ( Serial.available() ) {
-    if ( Serial.read() == SYN ) {
-      digitalWrite(GPIO_LED, 0);
-      
-      Serial.readBytes(bufstream, sizeof(bufstream));
-      memcpy(&cmdinit, bufstream, sizeof(bufstream));
-    
+    digitalWrite(GPIO_LED, 0);
+    elapsedtime_serial = millis();
+    stream = Serial.read();
+
+    while ( (stream == SYN) && (millis() - elapsedtime_serial) <= TIMEOUT ) {
+      if ( Serial.available() ) {
+        Serial.readBytes(bufstream, sizeof(bufstream));
+        memcpy(&cmdinit, bufstream, sizeof(bufstream));
+
 #ifdef DEBUG
-Serial.print("[CMD] Reading (bytes): ");Serial.println(sizeof(cmdinit), DEC);
-Serial.print("STX: ");Serial.println(cmdinit.stx, HEX);
-Serial.print("cmd: ");Serial.println(cmdinit.cmd, HEX);
-Serial.print("dt: ");Serial.println(cmdinit.param, DEC);
-Serial.print("ETX: ");Serial.println(cmdinit.etx, HEX);
+        Serial.print("[CMD] Reading (bytes): "); Serial.println(sizeof(cmdinit), DEC);
+        Serial.print("STX: "); Serial.println(cmdinit.stx, HEX);
+        Serial.print("cmd: "); Serial.println(cmdinit.cmd, HEX);
+        Serial.print("dt: "); Serial.println(cmdinit.param, DEC);
+        Serial.print("ETX: "); Serial.println(cmdinit.etx, HEX);
 #endif
 
-      if ( (cmdinit.stx == STX) && (cmdinit.etx == ETX) ) {
+        if ( (cmdinit.stx == STX) && (cmdinit.etx == ETX) ) {
 #ifdef DEBUG
-Serial.print("Incomme command: 0x");Serial.println(cmdinit.cmd, HEX);
-#endif 
+          Serial.print("Incomme command: 0x"); Serial.println(cmdinit.cmd, HEX);
+#endif
           switch ( cmdinit.cmd ) {
             case CMD_INIT:  CmdInit(&cmdinit);
-                            break;   
+              break;
             case CMD_UPD:   CmdUpdate(&cmdinit);
-                            break;
-            default: 
+              break;
+            case CMD_PAIR:  CmdPair();
+              break;
+            default:
 #ifdef DEBUG
-Serial.println("WHATTT???!!!!");
-#endif           
-                  break;
-          }
-      } else {
-#ifdef DEBUG
-Serial.println("Incomme serial: Nothing");
-#endif        
-      }
-
-      digitalWrite(GPIO_LED, 1);  
-    } else {
-#ifdef DEBUG
-Serial.println("[LOOP] No Sync");
-#endif       
-    }
-  }
-      
-  if ( digitalRead(GPIO_PAIR) ) {
-#ifdef DEBUG
-Serial.println("Pairing started...");
+              Serial.println("WHATTT???!!!!");
 #endif
+              break;
+          }
+        } else {
+#ifdef DEBUG
+          Serial.println("Incomme serial: Nothing");
+#endif
+        }
 
-    delay(DEBOUNCE);
-    simpleEspConnection.startPairing(120);  
+        break; //Exit while because do not anything to do
+      }
+    }
+
+    digitalWrite(GPIO_LED, 1);
   }
-  
 }
 
 
 /*
- * functions
- */
-void SerialFlush()
-{
-  while ( Serial.available()  ) {
-    Serial.read();    
-    delay(1);
-  }
-}
-
+   functions
+*/
 void CmdUpdate(SensoriandoWifiCommandInit *cmdinit)
-{ 
+{
 #ifdef DEBUG
-Serial.println("Command**********");
-Serial.print("STX: ");Serial.println(cmdinit->stx, HEX);
-Serial.print("cmd: ");Serial.println(cmdinit->cmd, HEX);
-Serial.print("dt: ");Serial.println(cmdinit->param, DEC);
-Serial.print("ETX: ");Serial.println(cmdinit->etx, HEX);
-Serial.println();
+  Serial.println("Command**********");
+  Serial.print("STX: "); Serial.println(cmdinit->stx, HEX);
+  Serial.print("cmd: "); Serial.println(cmdinit->cmd, HEX);
+  Serial.print("dt: "); Serial.println(cmdinit->param, DEC);
+  Serial.print("ETX: "); Serial.println(cmdinit->etx, HEX);
+  Serial.println();
 #endif
 
   if ( cmdinit->cmd = CMD_UPD) {
-      DtNow = cmdinit->param;
-      timeelapsed = millis();
+    DtNow = cmdinit->param;
+    timeelapsed = millis();
   }
 }
 
+void CmdPair()
+{
+#ifdef DEBUG
+  Serial.println("Pairing started...");
+#endif
+  simpleEspConnection.startPairing(TIMEPAIR);
+}
+
 void CmdInit(SensoriandoWifiCommandInit *cmdinit)
-{ 
+{
   SensoriandoWifiCommandResult cmdresult;
 
 #ifdef DEBUG
-Serial.println("Command**********");
-Serial.print("STX: ");Serial.println(cmdinit->stx, HEX);
-Serial.print("cmd: ");Serial.println(cmdinit->cmd, HEX);
-Serial.print("dt: ");Serial.println(cmdinit->param, DEC);
-Serial.print("ETX: ");Serial.println(cmdinit->etx, HEX);
-Serial.println();
+  Serial.println("Command**********");
+  Serial.print("STX: "); Serial.println(cmdinit->stx, HEX);
+  Serial.print("cmd: "); Serial.println(cmdinit->cmd, HEX);
+  Serial.print("dt: "); Serial.println(cmdinit->param, DEC);
+  Serial.print("ETX: "); Serial.println(cmdinit->etx, HEX);
+  Serial.println();
 #endif
 
   cmdresult.stx = STX;
   cmdresult.etx = ETX;
   cmdresult.res = NAK;
   cmdresult.cmd = cmdinit->cmd;
-  
-  if ( cmdinit->cmd = CMD_INIT) {
-      DtNow = cmdinit->param;
-      timeelapsed = millis();
-      cmdresult.res = ACK;    
-  }
-  
+
+  if ( cmdinit->cmd == CMD_INIT) {
+    cmdresult.res = ACK;
+
+    Serial.write(SYN);
+    Serial.write((uint8_t *)&cmdresult, sizeof(cmdresult));
+
 #ifdef DEBUG
-Serial.println("Result************");
-Serial.print("STX: ");Serial.println(cmdresult.stx, HEX);
-Serial.print("cmd: ");Serial.println(cmdresult.cmd, HEX);
-Serial.print("result: ");Serial.println(cmdresult.res, DEC);
-Serial.print("ETX: ");Serial.println(cmdresult.etx, HEX);
-Serial.println();
-#endif    
+    Serial.println("Result************");
+    Serial.print("STX: "); Serial.println(cmdresult.stx, HEX);
+    Serial.print("cmd: "); Serial.println(cmdresult.cmd, HEX);
+    Serial.print("result: "); Serial.println(cmdresult.res, DEC);
+    Serial.print("ETX: "); Serial.println(cmdresult.etx, HEX);
+    Serial.println();
+#endif
 
-  SerialFlush();
-
-  Serial.write(SYN);
-  delay(1);
-  Serial.write((uint8_t *)&cmdresult, sizeof(cmdresult));
+    DtNow = cmdinit->param; //Variable setted as init
+    timeelapsed = millis();
+  }
 }
 
 void OnSendError(uint8_t* ad)
 {
-  Serial.println("Sending to '"+simpleEspConnection.macToStr(ad)+"' was not possible!");  
+  Serial.println("Sending to '" + simpleEspConnection.macToStr(ad) + "' was not possible!");
 }
 
 void OnMessage(uint8_t* ad, const uint8_t* message, size_t len)
 {
-  digitalWrite(GPIO_LED, 0);
-  
   memcpy(&myData, message, sizeof(myData));
   myData.dt = DtNow;
 
 #ifdef DEBUG
-Serial.print("From client: ");Serial.println(simpleEspConnection.macToStr(ad));
-Serial.print("Bytes received: ");Serial.println(len, DEC);
-Serial.print("STX: ");Serial.println(myData.stx, HEX);
-Serial.print("id: ");Serial.println(myData.id, DEC);
-Serial.print("value: ");Serial.println(myData.value, DEC);
-Serial.print("dt: ");Serial.println(myData.dt, DEC);
-Serial.print("ETX: ");Serial.println(myData.etx, HEX);
-Serial.println();
+  Serial.println(""); Serial.println("[OnMessage]");
+  Serial.print("From client: "); Serial.println(simpleEspConnection.macToStr(ad));
+  Serial.print("Bytes received: "); Serial.println(len, DEC);
+  Serial.print("STX: "); Serial.println(myData.stx, HEX);
+  Serial.print("id: "); Serial.println(myData.id, DEC);
+  Serial.print("value: "); Serial.println(myData.value, DEC);
+  Serial.print("dt: "); Serial.println(myData.dt, DEC);
+  Serial.print("ETX: "); Serial.println(myData.etx, HEX);
 #endif
 
   if (myData.dt != NULL) {
-      Serial.write(SYN);
-      delay(1);
-      Serial.write((uint8_t *)&myData, sizeof(myData));
+    digitalWrite(GPIO_LED, 0);
+    Serial.write(SYN);
+    Serial.write((uint8_t *)&myData, sizeof(myData));
+    digitalWrite(GPIO_LED, 1);
   } else {
+
 #ifdef DEBUG
-Serial.println("Do not init!!!");
-#endif     
+    Serial.println(">> Do not init!!!");
+#endif
+
   }
-  
-  digitalWrite(GPIO_LED, 1);  
 }
 
 void OnPaired(uint8_t *ga, String ad)
 {
-  Serial.println("EspNowConnection : Client '"+ad+"' paired! ");
 
-  simpleEspConnection.endPairing();  
+#ifdef DEBUG
+  Serial.println("EspNowConnection : Client '" + ad + "' paired! ");
+#endif
+
+  simpleEspConnection.endPairing();
 }
 
 void OnConnected(uint8_t *ga, String ad)
 {
-    Serial.println(">> ");Serial.print(ad);
-    Serial.println(">>[CLIENT] connected!");
+
+#ifdef DEBUG
+  Serial.println(">> "); Serial.print(ad);
+  Serial.println(">>[CLIENT] connected!");
+#endif
+
 }
- 
